47ed285ab017913f59f08a16cd16e67bb9ad4000
박규열 <parkkyuyeol@baggyuyeol-ui-MacBook-Pro.local>
2019-09-27 05:34:11 +0900

    tlqnfjf

--- lanedetection/laneDetection.cpp
+++ lanedetection/laneDetection.cpp
@@ -6,40 +6,61 @@
 
 #include <vector>
 
-// 영상에서 선분을 추출하여 lines에 저장
-void findLines(Mat &image, vector<Vec4i> &lines) {
-    // Hough Transform 파라미터
-    float rho = 2; // distance resolution in pixels of the Hough grid
-    float theta = 1 * CV_PI / 180; // angular resolution in radians of the Hough grid
-    int hough_threshold = 100;    // minimum number of votes(intersections in Hough grid cell)
-    float minLineLength = 400; //minimum number of pixels making up a line
-    float maxLineGap = 30;   //maximum gap in pixels between connectable line segments
 
-    // 직선 검출 (Hough Transform)
-    HoughLinesP(image, lines, rho, theta, hough_threshold, minLineLength, maxLineGap);
+// 영상에서 선분을 추출하여 lines에 저장
+int findLines(Mat &image) {
+    int width = image.cols;
+    int height = image.rows;
+
+    auto* data = (uchar*)image.data;
+    int count = 0;
+    for (int x = 0; x < width; x++) {
+        count = 0;
+        for (int y = 0; y < height; y++) {
+            if (data[y*width + x] == 255) count++;
+        }
+        if (count >= (int)(height/2)) {
+            return x;
+        }
+        cout << "x, count, width, height : " << x << " " << count << " " << width << " " << height << endl;
+    }
+    return -1;
 }
 
 
-//vector<Vec2i> searchLine(Mat warpframe, int pivot, int step, int new_width, int new_height) {
-//    int slide_num = 10;
-//    int gap = (int)((float)new_height/(float)slide_num); // 각 슬라이드의 높이
-//    vector<Vec4i> lines[slide_num]; // 매 슬라이드 별 line들을 저장할 벡터
-//    // 한 슬라이드씩 올라가면서 차선 검색
-//    for (int step = 0; step < slide_num; step--) {
-//        Mat slide_frame = warpframe(Range(0, new_width),Range(new_height - (step + 1)*gap, new_height - step*gap));
-//        findLines(slide_frame, lines[step]);
-//
-//        // 검출이 되면, 다음 슬라이드로 넘어가서 다시 실행
-//        if (lines[step].size() != 0) {
-//
-//
-//
-//        }
-//
-//
-//    }
-//
-//}
+// 흑백 이진 영상에서 오브젝트 경계를 검출
+void findBoundary(Mat &input) {
+    vector<vector<Point>> contours;
+    findContours(input, contours, RETR_LIST, CHAIN_APPROX_NONE);
+    input = Mat::zeros(input.rows, input.cols, CV_8UC1);
+    drawContours(input, contours, -1, 255, 5);
+}
+
+
+void searchLine(const Mat& frame, vector<Vec2i> &line_points, int pivot, int step, int gap, int slide_num, int new_width, int new_height) {
+    vector<Vec4i> lines; // 검출된 직선들을 저장할 벡터
+
+    // 검색할 상위 영역 프레임 추출
+    int x_gap = 200; // 위로 슬라이딩하여 검색할 영역의 좌우 크기
+    int x_min = (pivot - x_gap >= 0) ? pivot - x_gap : 0;
+    int x_max = (pivot + x_gap <= new_width) ? pivot + x_gap : new_width;
+    int y_min = new_height - (step + 1) * gap;
+    int y_max = new_height - step * gap;
+    Mat search_frame = frame(Range(y_min, y_max), Range(x_min, x_max));
+    // 추출한 프레임에 대해 선분 검색
+    int new_pivot = findLines(search_frame);
+    cout << "pivot : " << pivot << " " << "new pivot : " << new_pivot << endl;
+//    imshow("search", search_frame);
+//    waitKey();
+    // 선분 검출이 되면, 해당 선분의 상단점을 차선 위 점으로 추가
+    if (new_pivot != -1) {
+        cout << "pivot : " << pivot << "new pivot : " << new_pivot << endl;
+        line_points.push_back(Point2i(x_min + new_pivot, y_min));
+        if (step + 1 < slide_num)
+            searchLine(frame, line_points, new_pivot, step + 1, gap, slide_num,  new_width, new_height);
+        else return;
+    } else return;
+}
 
 
 
@@ -55,9 +76,6 @@ void laneDetection(Mat &srcRGB, int iw, int ih, Mat &dstRGB, int nw, int nh) {
 //    float width = iw;
 //    float height = ih;
 
-    // 입력 영상을 자를 ROI 영역 지정
-    float rec_width = 1.0;
-    float rec_height = 0.5;
     cv::Rect myROI(0, (int)((float)height*0.5), width, (int)((float)height*0.5)); // (x,y,w,h)
     // ROI 영역으로 자르기
     frame = frame(myROI);
@@ -66,7 +84,7 @@ void laneDetection(Mat &srcRGB, int iw, int ih, Mat &dstRGB, int nw, int nh) {
 
     // Adaptive 이진화 및 잡음제거
     medianBlur(frame, frame, 5);
-    adaptiveThreshold(frame, frame, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, 151, -30);
+    adaptiveThreshold(frame, frame, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, 151, -40);
     erode(frame, frame, getStructuringElement(MORPH_RECT, Size(10, 10)));
     dilate(frame, frame, getStructuringElement(MORPH_RECT, Size(20, 20)));
 
@@ -79,7 +97,8 @@ void laneDetection(Mat &srcRGB, int iw, int ih, Mat &dstRGB, int nw, int nh) {
     corners[1] = Point2f(new_width, 0); // right top
     corners[2] = Point2f(0, new_height); // left bot
     corners[3] = Point2f(new_width, new_height); // right bot
-    Size warpSize(new_width, (float)new_width*height_ratio);
+    new_height = (int)((float)new_width*height_ratio);
+    Size warpSize(new_width, new_height);
     Mat warpframe(warpSize, frame.type());
     // Warping 후의 좌표
     vector<Point2f> warpCorners(4);
@@ -96,13 +115,80 @@ void laneDetection(Mat &srcRGB, int iw, int ih, Mat &dstRGB, int nw, int nh) {
     imshow("warp", warpframe);
     waitKey();
 
+    cout << "fuck0" << endl;
+
+    //findBoundary(warpframe);
+    // 곡선 차선 검출해서 line_points에 저장
+    int is_found = 0;
+    int slide_num = 10;
+    int gap = (int)((float)new_height/(float)slide_num); // 각 슬라이드의 높이
+    vector<Vec2i> line_points; // 직선 위 점들을 저장할 벡터
+    vector<Vec4i> lines;
+    cout << new_width << " " << new_height << " " << gap << endl;
+    cout << warpframe.cols << " " << warpframe.rows << " " << warpframe.type() << endl;
+    cout << 0 << " " << new_width << " " << new_height - gap << " " << new_height << endl;
+    Mat pivot_frame = warpframe(Range(new_height - gap, new_height), Range(0, new_width));
+//    Mat pivot_frame = warpframe(Rect(0, new_height, new_width, gap));
+    cout << "fuck0.5" << endl;
+
+    vector<int> pivots;
+    auto* data = (uchar*)pivot_frame.data;
+    int count = 0;
+    for (int x = 0; x < pivot_frame.cols; x++) {
+        count = 0;
+        for (int y = 0; y < pivot_frame.rows; y++) {
+            if (data[y*pivot_frame.cols + x] == 255) count++;
+        }
+        if (count >= (int)(pivot_frame.rows/2)) {
+            pivots.push_back(x);
+        }
+    }
+    cout << "fuck1" << endl;
+    if (!pivots.empty()) {
+        cout << "fuck2" << endl;
+
+        for (auto & pivot : pivots) {
+            cout << "pivot : " << pivot << endl;
+            line_points.push_back(Point2i(pivot, new_height - gap));
+            searchLine(warpframe, line_points, pivot, 1, gap, slide_num, new_width, new_height);
+            cout << "fuck4" << endl;
+            // 충분히 긴 직선이 검출되면 해당 선을 추종 차선으로 선택, 아니면 다시 실행
+            if (line_points.size() >= 3) {
+                is_found = 1;
+                break;
+            }
+            else {
+                line_points.clear();
+                continue;
+            }
+        }
+    }
+    cvtColor(warpframe, warpframe, COLOR_GRAY2BGR);
+    for (int i = 0; i < (int)line_points.size() - 1; i++) {
+        line(warpframe, line_points[i], line_points[i+1], Scalar(0, 0, 255), 20);
+    }
+
+    imshow("detect", warpframe);
+    waitKey();
+//    if (is_found) {
+//        for (auto & point : line_points)
+//            circle(warpframe, point, 5, Scalar(0, 0, 255), 2);
+//        imshow("detect", warpframe);
+//        waitKey();
+//    }
 
 //
-//    int slide_num = 10;
-//    int gap = (int)((float)new_height/(float)slide_num); // 각 슬라이드의 높이
-//    vector<Vec4i> lines[slide_num]; // 매 슬라이드 별 line들을 저장할 벡터
+//
+//
+//    for (int i = 0; i < (int)lines.size(); i++) {
+//
+//
+//    }
+//
+//
+//
 //    // 한 슬라이드씩 올라가면서 차선 검색
-//    for (int step = 0; step < slide_num; step--) {
+//    for (int step = 0; step < slide_num; step++) {
 //        Mat slide_frame = warpframe(Range(0, new_width),Range(new_height - (step + 1)*gap, new_height - step*gap));
 //        findLines(slide_frame, lines[step]);
 //        if (lines[step].size() != 0)
@@ -223,10 +309,10 @@ void laneDetection(Mat &srcRGB, int iw, int ih, Mat &dstRGB, int nw, int nh) {
 //        }
 //    }
 //
-
-
-
-
+//
+//
+//
+//
 //
 //    // 관심영역 설정 (set ROI (X, Y, W, H)).
 //    Rect rect(static_cast<int>(width*(1-rec_width)/2), static_cast<int>(height*(1-rec_height)), rec_width * width, rec_height * height);
