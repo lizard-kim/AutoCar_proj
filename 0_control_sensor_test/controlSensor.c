4bf008d6c45d87eaf1c247099f13867e1a096e14
Junho <junho@dgist.ac.kr>
2019-12-06 16:06:28 +0900

    jun

--- 0_control_sensor_test/controlSensor.c
+++ 0_control_sensor_test/controlSensor.c
@@ -2,6 +2,9 @@
 #include "car_lib.h"
 #include <math.h>
 #include <time.h>
+/****************************************************************************** 
+ TODO:
+ 0cm, 10cm FINE TUNING, MAKE EACH FUNCTION OF PAL & T;
 /*******************************************************************************
  *  Defines
  * 1 = front
@@ -39,7 +42,11 @@ void parking();                                                                 
 void tunnel_adv();                   
 void tunnel_real();     
 void parparking();      
-void warmSensorTrigger();                                     //
+void warmSensorTrigger();    
+void warmSensorTrigger_tunnel();   
+void warmSensorTrigger_pal();                        
+void warmSensorTrigger_T();      //
+void parparking_adv();
 ///////////////////////////////////////////////////////////////////////////////////////
 
 
@@ -50,12 +57,44 @@ void main(void)
     double data;
     CarControlInit();
     PositionControlOnOff_Write(UNCONTROL);
+    SpeedControlOnOff_Write(CONTROL);
     // 5. distance sensor --------------------------------------------------------
+
+    gain = SpeedPIDProportional_Read();
+    gain = 50;
+    SpeedPIDProportional_Write(gain);
+
+    gain = SpeedPIDIntegral_Read();
+    gain = 50;
+    SpeedPIDIntegral_Write(gain);
+
+    gain = SpeedPIDDifferential_Read();
+    gain = 10;
+    SpeedPIDDifferential_Write(gain);
+
+
     printf("INPUT Number\n");
     scanf("%d", &channel);
     while(1)
     {
-        warmSensorTrigger();
+        if(channel == 1)
+        {
+            warmSensorTrigger();
+        }
+        else if(channel == 2)
+        {
+            warmSensorTrigger_pal();
+        }
+        else if(channel == 3)
+        {
+            //warmSensorTrigger_tunnel();
+            tunnel_adv();
+        }
+        else if(channel == 4)
+        {
+            warmSensorTrigger_T();
+        }
+        else{ printf("undefined\n");}
     }
 }
 
@@ -64,7 +103,7 @@ double DistFunc(double data)
 {
     I_Dist = data;
     O_Dist = pow(I_Dist/22691.0, -1/1.0706);
-    printf("%.0lf\n", O_Dist);
+    //printf("%.0lf\n", O_Dist);
     return O_Dist;
 }
 
@@ -96,7 +135,6 @@ void warmSensorTrigger() // must be included ParkingSignal_1, ParkingSignal_2, p
 {
    
 
-    printf("FUCKFUCK\n");
 
     DesireSpeed_Write(100);
     SteeringServoControl_Write(1500);
@@ -129,6 +167,7 @@ void warmSensorTrigger() // must be included ParkingSignal_1, ParkingSignal_2, p
         ParkingSignal_1 = 2;
         printf("Parking Area here\n");
         float endtime_1 = (clock() - start_1)/(CLOCKS_PER_SEC);
+        printf("endtime_1 : %d\n", endtime_1);
         if(endtime_1 > 10)
         {
             ParkingSignal_1 = 0;
@@ -156,6 +195,7 @@ void warmSensorTrigger() // must be included ParkingSignal_1, ParkingSignal_2, p
     {
         parParkingSignal_1 = 2;  
         float endtime_2 = (clock() - start_2)/(CLOCKS_PER_SEC);
+        printf("endtime_2 : %d\n", endtime_2);
         if(endtime_2 > 10)
         {
             parParkingSignal_1 = 0;
@@ -193,6 +233,162 @@ void warmSensorTrigger() // must be included ParkingSignal_1, ParkingSignal_2, p
 }
 
 
+void warmSensorTrigger_pal()
+{
+
+
+
+    DesireSpeed_Write(100);
+    SteeringServoControl_Write(1500);
+
+
+    I_data_1 = DistanceSensor(1);
+    O_data_1 = DistFunc(I_data_1);
+
+    I_data_2 = DistanceSensor(2);
+    O_data_2 = DistFunc(I_data_2);
+
+    I_data_3 = DistanceSensor(3);
+    O_data_3 = DistFunc(I_data_3);
+
+    I_data_4 = DistanceSensor(4);
+    O_data_4 = DistFunc(I_data_4);
+
+    clock_t start_1=0, start_2=0, start_3=0, end_2 = 0;
+    float endtime_1=0, endtime_2=0, endtime_3=0;
+
+
+    if(parParkingSignal_2 == 0 && parParkingSignal_1 == 0 && O_data_2 < 30 && O_data_3 > 30)
+    {
+        start_2 = clock();
+        parParkingSignal_1 = 1;
+    }
+    if(parParkingSignal_1 == 1 && O_data_2 > 50 && O_data_3 > 50)
+    {
+       // printf("INITIAL\n");
+        parParkingSignal_1 = 0;
+    }
+    if(parParkingSignal_1 == 1 && O_data_2 > 30 && O_data_3 < 30)    
+    {
+        parParkingSignal_1 = 2;  
+        end_2 = clock();
+        float endtime_2 = (end_2-start_2)/CLOCKS_PER_SEC;
+        printf("endtime_2 : %d\n", endtime_2);
+        if(endtime_2 > 10)
+        {
+            parParkingSignal_1 = 0;
+        }
+    }
+    if(parParkingSignal_1 == 2 && O_data_2 > 30 && O_data_3 > 30)
+    {
+        parParkingSignal_1 = 3;         
+    }
+    if(parParkingSignal_1 == 3 && O_data_3 < 30)
+    {
+        parparking();
+        parParkingSignal_2 = 2;
+        tunnelSignal = 1;
+    }
+
+}
+
+void warmSensorTrigger_tunnel()
+{
+
+    DesireSpeed_Write(100);
+    SteeringServoControl_Write(1500);
+
+
+    I_data_2 = DistanceSensor(2);
+    O_data_2 = DistFunc(I_data_2);
+
+    I_data_3 = DistanceSensor(3);
+    O_data_3 = DistFunc(I_data_3);
+
+    clock_t start_1=0, start_2=0, start_3=0;
+    float endtime_1=0, endtime_2=0, endtime_3=0;
+
+
+    if(tunnelSignal == 0 && O_data_2 < 30)
+    {
+        start_3 = clock();
+        tunnelSignal = 2;
+    }
+    if(tunnelSignal == 2 && O_data_2 < 30 && O_data_3 < 30)
+    {
+        float endtime_3 = (clock()- start_3)/(CLOCKS_PER_SEC);
+        printf("endtime_2 : %d\n", endtime_3);
+        if(endtime_3 < 3)
+        {
+            printf("clock function operated....\n");
+            tunnelSignal = 3;
+            tunnel_adv();
+        }
+        else
+        {
+            printf("clock function anoperated....\n");
+            tunnel_adv();
+            //tunnelSignal = 1;
+        }
+    }
+}
+
+
+
+
+void warmSensorTrigger_T()
+{
+     DesireSpeed_Write(100);
+    SteeringServoControl_Write(1500);
+
+
+    I_data_1 = DistanceSensor(1);
+    O_data_1 = DistFunc(I_data_1);
+
+    I_data_2 = DistanceSensor(2);
+    O_data_2 = DistFunc(I_data_2);
+
+    I_data_3 = DistanceSensor(3);
+    O_data_3 = DistFunc(I_data_3);
+
+    I_data_4 = DistanceSensor(4);
+    O_data_4 = DistFunc(I_data_4);
+
+    clock_t start_1=0, start_2=0, start_3=0;
+    float endtime_1=0, endtime_2=0, endtime_3=0;
+     
+    if(ParkingSignal_2 == 0 && ParkingSignal_1 == 0 && O_data_2 < 30 && O_data_3 > 30)
+    {
+        start_1 = clock();
+        ParkingSignal_1 = 1;
+        printf("Parking Point Detected...\n");
+       // continue;
+    }
+    if(ParkingSignal_1 == 1 && O_data_2 > 30 && O_data_3 < 30)
+    {
+        ParkingSignal_1 = 2;
+        printf("Parking Area here\n");
+        float endtime_1 = (clock() - start_1)/(CLOCKS_PER_SEC);
+        printf("endtime_1 : %d\n", endtime_1);
+        if(endtime_1 > 10)
+        {
+            ParkingSignal_1 = 0;
+        }
+        //continue;
+    }
+    if(ParkingSignal_1 == 2 && O_data_2 > 30 && O_data_3 > 30)
+    {
+        ParkingSignal_1 = 3;
+    }
+    if(ParkingSignal_1 == 3 && O_data_3 < 30)
+    {
+        parking();
+        ParkingSignal_2 = 1;
+        parParkingSignal_2 = 1;
+    }
+}
+
+
 void tunnel_adv()
 {
    // DesireSpeed_Write(200);
@@ -210,7 +406,7 @@ void tunnel_adv()
     printf("O_data_5 : %d", O_data_5);
     //Distance between car and wall is each 10cm.
     // 10 --> 1500 , 20 --> 1000            y = -50x + 2000
-    if(O_data_2 >= 10)
+    if(O_data_2 >= 8)
     {
         angle = -50*O_data_2 + 2000;
         if(angle < 1200)
@@ -221,17 +417,17 @@ void tunnel_adv()
         {
             DesireSpeed_Write(100);
             //angle = -16*O_data_1 + 2140;
-            angle = 1990;
+            angle = 1001;
         }
         SteeringServoControl_Write(angle);
         if(O_data_1 < 40)
         {
-            usleep(500000);
+            usleep(300000);
         }
         DesireSpeed_Write(200);
         printf("tunnel, O>10 = %d", angle);
     }
-    else if(O_data_2 < 10) // 10--> 1500 0 --> 1800            y = -30x + 1800
+    else if(O_data_2 < 8) // 10--> 1500 0 --> 1800            y = -30x + 1800
     {
         angle = -30*O_data_2 + 1800;
         if(O_data_1 < 40)
@@ -246,8 +442,9 @@ void tunnel_adv()
         {
             usleep(300000);
         }
-    }
-    else if(O_data_1 < 40) // must be edited afterward;;;;;;;;;;
+    } 
+    /////////////////////////////////////FROM HERE, DUMMY CODE!!! /////////////////////////////////////////
+    else if(O_data_1 < 40)
     {
         printf("tunnel, detected front sensor");
         if(O_data_2 <  10) // right curve , looking front
@@ -269,7 +466,7 @@ void tunnel_adv()
     else if(O_data_2 > 60)
     {
         return;
-    }
+    } 
 }
 
 
@@ -373,6 +570,8 @@ void parparking()
     I_data_3 = DistanceSensor(3);
     int I_data_5 = DistanceSensor(5);
     int O_data_5 = DistFunc(I_data_5);
+    int tmpdist_1 =  0;
+    int tmpdist = 0;
     O_data_2 = DistFunc(I_data_2);
     O_data_3 = DistFunc(I_data_3);
 
@@ -381,7 +580,13 @@ void parparking()
     //printf("ParkingSignal = %d\n", ParkingSignal_1);
     printf("O_data_2 : %d\n", O_data_2);
     printf("O_data_3 : %d\n", O_data_3);
-    usleep(180000);
+    tmpdist_1 = O_data_2;
+    if(tmpdist_1 >= 15)
+    {
+        tmpdist = tmpdist_1;
+    }
+    usleep(210000+(tmpdist*5000));
+    
     DesireSpeed_Write(0);
     //printf("Parking Start!!\n");
     usleep(500000);
@@ -389,24 +594,24 @@ void parparking()
     SteeringServoControl_Write(1000);
     //printf("Steering..\n.");
     EncoderCounter_Write(0);
-    DesireSpeed_Write(-100);
-    usleep(1400000);
+    DesireSpeed_Write(-50);
+    usleep(1300000+(tmpdist*5000));
     
     int posRead_1 = EncoderCounter_Read();
     printf("EncoderCounter_Read() = %d\n", posRead_1);
 
     printf("-600 reached\n");
     SteeringServoControl_Write(1500);
-    usleep(380000);
+    usleep(1800000);
 
     printf("-800 reached\n");
     SteeringServoControl_Write(2000);
 
     while(1)
     {
-        if(DistFunc(DistanceSensor(4)) > 8)
+        if(DistFunc(DistanceSensor(4)) > 7 && DistFunc(DistanceSensor(3)) > 4)
         {
-            DesireSpeed_Write(-100);
+            DesireSpeed_Write(-80);
         }
         else
         {
@@ -416,6 +621,7 @@ void parparking()
     }
 
     SteeringServoControl_Write(1500);
+    
     while(1)
     {
         int posRead_1 = DistFunc(DistanceSensor(4));
@@ -441,26 +647,265 @@ void parparking()
     */
 
     SteeringServoControl_Write(1999);
-    DesireSpeed_Write(100);
-    usleep(500000);
+    DesireSpeed_Write(80);
+    usleep(300000);
     printf("step 1...\n");
 
     SteeringServoControl_Write(1500);
-    DesireSpeed_Write(-100);
-    usleep(600000);
+    DesireSpeed_Write(-80);
+    usleep(300000);
     printf("step 1...\n");
 
     SteeringServoControl_Write(1999);
-    DesireSpeed_Write(100);
-    usleep(1600000);
+    DesireSpeed_Write(80);
+    usleep(700000);
     printf("step 1...\n");
 
     SteeringServoControl_Write(1500);
-    usleep(700000);
+    usleep(500000);
 
     SteeringServoControl_Write(1000);
+    usleep(1300000);
+    printf("step 2...\n");
+
+    SteeringServoControl_Write(1500);
+
+    printf("Basic Mode is ready...Parking finished..!!!\n");
+    printf("tmpdist : %d\n", tmpdist);
+    DesireSpeed_Write(0); //E-Stop;
+    parParkingSignal_1 = 0;
+    CarLight_Write(ALL_OFF);
+
+    //return 0;
+        
+}
+
+
+
+
+
+
+
+void parparking_adv()
+{
+    DesireSpeed_Write(100);
+    /*  After parking(), trgging parparking()...
+    if(parParkingSignal_2 != 1)
+    {
+        return;
+    }*/
+
+    I_data_2 = DistanceSensor(2);
+    I_data_3 = DistanceSensor(3);
+    int I_data_5 = DistanceSensor(5);
+    int O_data_5 = DistFunc(I_data_5);
+    O_data_2 = DistFunc(I_data_2);
+    O_data_3 = DistFunc(I_data_3);
+
+    CarLight_Write(REAR_ON);
+    printf("Fucking PARARREL\n");
+    //printf("ParkingSignal = %d\n", ParkingSignal_1);
+    printf("O_data_2 : %d\n", O_data_2);
+    printf("O_data_3 : %d\n", O_data_3);
+    usleep(180000);
+    DesireSpeed_Write(0);
+    //printf("Parking Start!!\n");
+    usleep(500000);
+
+    EncoderCounter_Write(0);
+   
+
+    EncoderCounter_Write(0);
+    DesireSpeed_Write(-100);
+    printf("EncoderCounter_Read() = %d\n", posRead);
+    SteeringServoControl_Write(1001);
+    while(1)
+    {
+        if(EncoderCounter_Read() < -1300) // or EncoderCounter_Read == -6000; it means car moved 30cm.
+        {
+            DesireSpeed_Write(0);
+            //SteeringServoControl_Write(1500);
+            //SteeringServoControl_Write(1500);
+            break;
+        }
+    }
+
+
+
+    /* 
+    SteeringServoControl_Write(1001);
+    DesireSpeed_Write(100);
+    usleep(1000000); // maximum right steering
+    */
+
+    EncoderCounter_Write(0);
+    DesireSpeed_Write(-100);
+    printf("EncoderCounter_Read() = %d\n", posRead);
+    SteeringServoControl_Write(1999);
+    while(1)
+    {
+        if(EncoderCounter_Read() < -1300) // or EncoderCounter_Read == -6000; it means car moved 30cm.
+        {
+            DesireSpeed_Write(0);
+            //SteeringServoControl_Write(1500);
+            break;
+        }
+    }
+
+
+    /* 
+    SteeringServoControl_Write(1999);
+    DesireSpeed_Write(-100);
+    usleep(1000000);
+    */
+
+
+    EncoderCounter_Write(0);
+    DesireSpeed_Write(100);
+    printf("EncoderCounter_Read() = %d\n", posRead);
+    SteeringServoControl_Write(1800);
+    while(1)
+    {
+        if(EncoderCounter_Read() > 1000) // or EncoderCounter_Read == -6000; it means car moved 30cm.
+        {
+            DesireSpeed_Write(0);
+            //SteeringServoControl_Write(1500);
+            break;
+        }
+    }
+
+
+
+   /* 
+    SteeringServoControl_Write(1800);
+    DesireSpeed_Write(100);
+    usleep(500000);
+*/
+
+
+
+
+    SteeringServoControl_Write(1500);
+    while(1)
+    {
+        if(DistFunc(DistanceSensor(4)) > 8)
+        {
+            DesireSpeed_Write(-100);
+        }
+        else
+        {
+            DesireSpeed_Write(0);
+            break;
+        }
+    } //Second back propagation
+
+
+
+    EncoderCounter_Write(0);
+    DesireSpeed_Write(100);
+    printf("EncoderCounter_Read() = %d\n", posRead);
+    SteeringServoControl_Write(1999);
+    while(1)
+    {
+        if(EncoderCounter_Read() > 1000) // or EncoderCounter_Read == -6000; it means car moved 30cm.
+        {
+            DesireSpeed_Write(0);
+            //SteeringServoControl_Write(1500);
+            break;
+        }
+    }
+
+    /* 
+    SteeringServoControl_Write(1999);
+    DesireSpeed_Write(100);
+    usleep(500000);
+    printf("step 1...\n");
+    */
+
+    EncoderCounter_Write(0);
+    DesireSpeed_Write(-100);
+    printf("EncoderCounter_Read() = %d\n", posRead);
+    SteeringServoControl_Write(1001);
+    while(1)
+    {
+        if(EncoderCounter_Read() < -1100) // or EncoderCounter_Read == -6000; it means car moved 30cm.
+        {
+            DesireSpeed_Write(0);
+            //SteeringServoControl_Write(1500);
+            break;
+        }
+    }
+    /* 
+    SteeringServoControl_Write(1001);
+    DesireSpeed_Write(-100);
+    usleep(600000);
+    printf("step 1...\n");
+    */
+
+    EncoderCounter_Write(0);
+    DesireSpeed_Write(100);
+    printf("EncoderCounter_Read() = %d\n", posRead);
+    SteeringServoControl_Write(1500);
+    while(1)
+    {
+        if(EncoderCounter_Read() > 1800) // or EncoderCounter_Read == -6000; it means car moved 30cm.
+        {
+            DesireSpeed_Write(0);
+            //SteeringServoControl_Write(1500);
+            break;
+        }
+    }
+
+    /* 
+    SteeringServoControl_Write(1500);
+    DesireSpeed_Write(100);
+    usleep(1600000);
+    printf("step 1...\n");
+    */
+
+    EncoderCounter_Write(0);
+    DesireSpeed_Write(100);
+    printf("EncoderCounter_Read() = %d\n", posRead);
+    SteeringServoControl_Write(1001);
+    while(1)
+    {
+        if(EncoderCounter_Read() > 1200) // or EncoderCounter_Read == -6000; it means car moved 30cm.
+        {
+            DesireSpeed_Write(0);
+            //SteeringServoControl_Write(1500);
+            break;
+        }
+    }
+
+
+    /* 
+    SteeringServoControl_Write(1001);
+    usleep(700000);
+    */
+
+
+    EncoderCounter_Write(0);
+    DesireSpeed_Write(100);
+    printf("EncoderCounter_Read() = %d\n", posRead);
+    SteeringServoControl_Write(1500);
+    while(1)
+    {
+        if(EncoderCounter_Read() > 2200) // or EncoderCounter_Read == -6000; it means car moved 30cm.
+        {
+            DesireSpeed_Write(0);
+            //SteeringServoControl_Write(1500);
+            break;
+        }
+    }
+
+
+    /* 
+    SteeringServoControl_Write(1500);
     usleep(2000000);
     printf("step 2...\n");
+    */
+
+
 
     SteeringServoControl_Write(1500);
 
